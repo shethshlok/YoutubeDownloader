6efd80dcefbcb01dcf6f239f46f7eca8bc5a057c
Shlok Sheth <ss3756@nau.edu>
2024-07-25 23:31:30 +0530

    test 8

--- app.py
+++ app.py
@@ -1,19 +1,16 @@
-import os
 from flask import Flask, render_template, request, redirect, url_for
 from pytubefix import YouTube
 from moviepy.editor import VideoFileClip, AudioFileClip
 import re
+import os
 
 app = Flask(__name__)
-
-# Set the IMAGEIO_FFMPEG_EXE environment variable to the path of the ffmpeg binary
-os.environ["IMAGEIO_FFMPEG_EXE"] = "/.ffmpeg/ffmpeg"
 
 # Regex pattern for validating YouTube URLs
 youtube_regex = re.compile(r'(https?://)?(www\.)?(youtube|youtu|youtube-nocookie)\.(com|be)/.+')
 
 # Path to the Downloads directory
-downloads_path = os.path.join(os.path.expanduser("~"), "Downloads")
+downloads_path = '/tmp'
 
 @app.route('/')
 def index():
@@ -46,7 +43,7 @@ def download():
         yt = YouTube(url)
         video_title = yt.title
         safe_title = "".join([c if c.isalnum() else "_" for c in video_title])  # Make the title filename-safe
-        
+
         if format == 'mp3':
             audio_stream = yt.streams.filter(adaptive=True, file_extension='mp4', only_audio=True).first()
             audio_path = audio_stream.download(output_path=downloads_path, filename=f'{safe_title}.mp4')
@@ -54,26 +51,27 @@ def download():
             audio_clip = AudioFileClip(audio_path)
             audio_clip.write_audiofile(os.path.join(downloads_path, f'{safe_title}.mp3'))
             os.remove(audio_path)
+            return send_file(os.path.join(downloads_path, f'{safe_title}.mp3'), as_attachment=True)
         else:
             video_stream = yt.streams.filter(adaptive=True, file_extension='mp4', only_video=True, resolution=resolution).first()
             video_path = video_stream.download(output_path=downloads_path, filename=f'{safe_title}_video.mp4')
             audio_stream = yt.streams.filter(adaptive=True, file_extension='mp4', only_audio=True).first()
             audio_path = audio_stream.download(output_path=downloads_path, filename=f'{safe_title}_audio.mp4')
-            
+
             # Merge video and audio using moviepy
             video_clip = VideoFileClip(video_path)
             audio_clip = AudioFileClip(audio_path)
             final_clip = video_clip.set_audio(audio_clip)
             output_path = os.path.join(downloads_path, f'{safe_title}.mp4')
             final_clip.write_videofile(output_path, codec='libx264')
-            
+
             # Delete the separate video and audio files
             os.remove(video_path)
             os.remove(audio_path)
 
-        return redirect(url_for('index', success=True))
+            return send_file(output_path, as_attachment=True)
     except Exception as e:
         return redirect(url_for('index', error=str(e)))
 
 if __name__ == '__main__':
-    app.run(debug=False, host='0.0.0.0')
+    app.run(debug=False)
